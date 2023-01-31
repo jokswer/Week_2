056225dd331b921e403406b2a55023df9f26e7b4
jokswer <jokswer@gmail.com>
2023-01-31 23:52:30 +0700

    feat: rename fields

--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 public class Root : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
+    [SerializeField] private CameraMovement _cameraMovement;
     
     [Header("Player Config")] 
     [SerializeField] private Transform _playerStartPoint;
     [SerializeField] private float _playerSpeed = 5f;
 
     private PlayerPresenter _playerPresenter;
+    private Transform _playerTransform;
 
     private void Start()
     {
-        var playerInput = new PlayerModel(_playerSpeed);
+        var playerModel = new PlayerModel(_playerSpeed);
         var playerObject = Instantiate(_player, _playerStartPoint.position, Quaternion.identity);
         var playerView = playerObject.GetComponent<PlayerView>();
 
-        _playerPresenter = new PlayerPresenter(playerView, playerInput);
+        _playerPresenter = new PlayerPresenter(playerView, playerModel);
+        _playerTransform = playerObject.GetComponent<Transform>();
     }
 
     private void Update()
@@ -28,5 +31,10 @@ public class Root : MonoBehaviour
     private void FixedUpdate()
     {
         _playerPresenter.FixedUpdate();
+    }
+
+    private void LateUpdate()
+    {
+        _cameraMovement.SetPosition(_playerTransform.position);
     }
 }
